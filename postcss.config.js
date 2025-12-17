ff9942c614728a7467d1c1b9c41502f6b9157763
Md. Monjurul Haque <himel452@gmail.com>
2025-12-17 19:55:27 +0600

    fixed base path for vercel

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // পরিবর্তন
     autoprefixer: {},
   },
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ecohut/", // <--- এই লাইনটি খুব গুরুত্বপূর্ণ! আপনার রিপোজিটরির নাম দিন
+  base: '/',  // <--- এই লাইনটি খেয়াল করুন, এটি '/' হতে হবে
 })
